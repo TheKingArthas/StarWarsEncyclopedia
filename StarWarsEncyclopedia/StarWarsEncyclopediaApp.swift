ce7ff3d7bc99efd300dcc80d7acf81271c5d4a31
TheKingArthas <federicondeluca@gmail.com>
2023-10-19 20:56:54 -0300

    Initial Commit

---
+++ StarWarsEncyclopedia/StarWarsEncyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsEncyclopediaApp.swift
+//  StarWarsEncyclopedia
+//
+//  Created by Personal on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsEncyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
