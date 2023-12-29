b4b68c61a43f5478d4427c3897cc2ac483af114e
Federico De Luca <federicondeluca@gmail.com>
2023-12-29 02:02:40 -0300

    Character model created

---
+++ StarWarsEncyclopedia/Models/CharacterModel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct CharacterModel {
+    var name: String
+    var height: Int
+    var weight: Int
+    var hairColor: String
+    var skinColor: String
+    var eyesColor: String
+    var birthYear: String
+    var gender: String
+    var homeWorld: String
+    var species: String
+    var imagePaths: String
+}
