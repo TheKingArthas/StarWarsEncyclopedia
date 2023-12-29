aa2d45b800ad6072f0adcad35ff285b2783017f1
Federico De Luca <federicondeluca@gmail.com>
2023-12-29 03:44:21 -0300

    Character view UI improvements

--- StarWarsEncyclopedia/Presentation/Views/CharacterView.swift
+++ StarWarsEncyclopedia/Presentation/Views/CharacterView.swift
@@ -17,38 +17,56 @@ struct CharacterView: View {
                 .ignoresSafeArea()
             VStack {
                 characterAvatarView()
+                    .padding([.top, .bottom], 32)
                 characterInfoView()
+                    .padding(.bottom, 32)
             }
         }
     }
     
     private func backgroundView() -> some View {
-        LinearGradient(gradient: Gradient(colors: [Color.black,
-                                                   Color.teal,
-                                                   Color.white]),
-                       startPoint: .top,
-                       endPoint: .bottom)
+        ZStack {
+            Image("SpaceBackground")
+                .opacity(0.9)
+            LinearGradient(gradient: Gradient(colors: [Color.clear,
+                                                       Color.black,
+                                                       Color.gray]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+        }
     }
     
     private func characterInfoView() -> some View {
         VStack(alignment: .leading, spacing: 8) {
-            Text("Name: \(character.name)")
-            HStack {
-                Text("Gender: ")
-                genderImageView(character.gender)
+            Text("\(character.name)")
+                .font(.system(size: 32))
+                .foregroundStyle(.white)
+            if character.gender != .other {
+                HStack {
+                    Text("Gender: ")
+                        .foregroundStyle(.white)
+                    genderImageView(character.gender)
+                }
             }
             Text("Birth year: \(character.birthYear)")
+                .foregroundStyle(.white)
             Text("Height: \(character.height)")
+                .foregroundStyle(.white)
             Text("Weight: \(character.weight)")
+                .foregroundStyle(.white)
             Text("Hair color: \(character.hairColor)")
+                .foregroundStyle(.white)
             Text("Skin color: \(character.skinColor)")
+                .foregroundStyle(.white)
             Text("Eyes color: \(character.eyesColor)")
+                .foregroundStyle(.white)
         }
     }
     
     private func characterAvatarView() -> some View {
         Image(systemName: "person.fill")
             .resizable()
+            .background(Color.white)
             .aspectRatio(contentMode: .fill)
             .frame(width: 256, height: 256)
             .clipShape(Circle())
@@ -62,10 +80,14 @@ struct CharacterView: View {
         case .male:
             Image("gender_male")
                 .resizable()
+                .renderingMode(.template)
+                .foregroundStyle(.white)
                 .frame(width: frameSize, height: frameSize)
         case .female:
             Image("gender_female")
                 .resizable()
+                .renderingMode(.template)
+                .foregroundStyle(.white)
                 .frame(width: frameSize, height: frameSize)
         default:
             EmptyView()
